49e42e7a85c31f9dd2975a7b4efff9a4383784c3
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-03-16 17:24:19 -0700

    New commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	cobst char str1[] = "\" - Dora Korpar, 2015-10-19\n";
+	const char str1[] = "\" - Dora Korpar, 2015-10-19\n";
 	const char str[] = "and that piece of art is useful" + str1;
 
 	write(2, str, sizeof(str) - 1);
