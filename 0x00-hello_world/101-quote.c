ef914b2f41fe40654cea07dc47e29879d7a767dd
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-03-16 17:49:17 -0700

    main.s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-	const char str1[] = "\" - Dora Korpar, 2015-10-19\n";
-	const char str[] = "and that piece of art is useful" + str1;
+	const char str[] = "and that piece of art is useful\" - Dora Korpar, "
+				"2015-10-19\n";
 
 	write(2, str, sizeof(str) - 1);
 	return (1);
