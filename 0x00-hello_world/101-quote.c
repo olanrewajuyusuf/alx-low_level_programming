c500e09a569861604bce876e31a665e2e16bddb9
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-03-16 17:21:18 -0700

    New commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * write to standard error (file descriptor 2)
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	cobst char str1[] = "\" - Dora Korpar, 2015-10-19\n";
+	const char str[] = "and that piece of art is useful" + str1;
+
+	write(2, str, sizeof(str) - 1);
+	return (1);
+}
