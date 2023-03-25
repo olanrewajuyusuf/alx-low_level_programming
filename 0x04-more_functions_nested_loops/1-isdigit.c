7ea0b232267381fabb43f12c3902d308725a65ac
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-03-25 03:50:22 -0700

    Files added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - check if the parameter is digit
+ *
+ * @c: argument
+ *
+ * Return: 1 if its digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
