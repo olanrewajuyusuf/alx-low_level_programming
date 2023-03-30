9dd2823fe8813bca1c03b3249cec2b76d59ce75f
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-03-30 14:23:02 -0700

    Three files

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: str
+ * @src: str
+ * @n: bytes num
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	src_len++;
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+	dest[index] = '\0';
+	return (dest);
+}
