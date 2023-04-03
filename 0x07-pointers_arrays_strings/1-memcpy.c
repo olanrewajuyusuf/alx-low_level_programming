3cd6059623b5a2ec10889bfff79acbd63f175902
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-04-03 05:35:44 -0700

    Correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * _memcpy - copies memory area.
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchar - locates a character in a string.
@@ -17,5 +18,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	return (null);
+	return (NULL);
 }
