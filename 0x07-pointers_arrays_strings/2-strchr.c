5d9c625145c321258d85556c4b7e3e5f2470cb61
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-04-03 05:30:29 -0700

    Other two files

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchar - locates a character in a string.
+ * @s: pointer
+ * @c: character
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (null);
+}
