080feaa2d69a742431c5cb294169ccb5333a3bc9
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-06-07 00:51:35 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: character to print
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
