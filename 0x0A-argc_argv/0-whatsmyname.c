cfb5512f7e555ff71debdd34ac19519177db9112
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-06-10 21:22:12 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,16 +10,6 @@
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	char *programName;
-	char *slash;
-
-	programName = argv[0];
-	slash = strrchr(programName, '/');
-	if (slash != NULL)
-	{
-		programName = slash + 1;
-	}
-
-	printf("%s\n", programName);
+	printf("%s\n", argv[0]);
 	return (0);
 }
