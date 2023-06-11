ddd1ddba9998a038bc91b37042f66951aa7941ac
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-06-11 08:09:39 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,16 +19,17 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-			if (isdigit(argv[i][j]))
-				sum += atoi(argv[i]);
-			else
-			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			}
+			sum += atoi(argv[i]);
 		}
+		printf("%d\n", sum);
 	}
-	printf("0\n");
+	else
+		printf("0\n");
 	return (0);
 }
