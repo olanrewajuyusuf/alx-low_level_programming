b70bd5949eb3493a151049a94d1396e207430e79
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-06-11 07:31:13 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: argument vector
  * Return: Success
  */
-int main(__attribute__((unused))int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
