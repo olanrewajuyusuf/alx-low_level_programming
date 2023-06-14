1556c2d72e48126ca9ade2667644a42123cad5a9
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-06-14 16:38:06 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *  which contains a copy of the string given as a parameter.
+ *  @str: string to duplicate
+ *  Return: NULL or duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup_arr;
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	dup_arr = malloc(sizeof(char) * len + 1);
+	if (dup_arr == NULL)
+		return (NULL);
+	dup_arr = strdup(str);
+	return (dup_arr);
+}
