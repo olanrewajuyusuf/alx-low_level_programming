121a79e0bed63d0e9d4b2ee4668481dcd88c3927
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-06-14 17:01:48 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,18 @@
 char *_strdup(char *str)
 {
 	char *dup_arr;
-	int len;
+	int len, i;
 
 	if (str == NULL)
 		return (NULL);
 	len = 0;
 	while (str[len] != '\0')
 		len++;
-	dup_arr = (char*)malloc(sizeof(char) * (len + 1));
+	dup_arr = (char *)malloc(sizeof(char) * (len + 1));
 	if (dup_arr == NULL)
 		return (NULL);
-	dup_arr = strdup(str);
+	for (i = 0; i < len; i++)
+		dup_arr[i] = str[i];
+	dup_arr[len] = '\0';
 	return (dup_arr);
 }
