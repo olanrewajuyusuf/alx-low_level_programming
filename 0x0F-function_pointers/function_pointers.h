a47e4b63d6a75865056c0dcbe398b883b8ac800f
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-02-17 08:58:47 -0800

    2-int_index.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUCTION_POINTER_H
 #define FUCTION_POINTER_H
+#include <stddef.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
